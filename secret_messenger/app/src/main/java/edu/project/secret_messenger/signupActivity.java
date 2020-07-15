f36c33474a4224abeb54e5f6d7611e0e86a9269b
kooym5607 <starkoo06@kookmin.ac.kr>
2020-07-15 10:21:32 +0900

    회원가입 ID 중복체크

--- secret_messenger/app/src/main/java/edu/project/secret_messenger/signupActivity.java
+++ secret_messenger/app/src/main/java/edu/project/secret_messenger/signupActivity.java
@@ -6,12 +6,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.security.InvalidKeyException;
+import java.util.EventListener;
 
 import static edu.project.secret_messenger.util.*;
 
@@ -20,9 +26,10 @@ public class signupActivity extends AppCompatActivity {
     private EditText pwEdit;
     private EditText nameEdit;
     private Button signupFinishBtn;
+    private Button sameidBtn;
     private FirebaseDatabase database= FirebaseDatabase.getInstance();
     private FirebaseAdapter DB;
-    private DatabaseReference ref;
+    private DatabaseReference ref = database.getReference();
     private User user;
 
     @Override
@@ -33,6 +40,13 @@ public class signupActivity extends AppCompatActivity {
         idEdit = (EditText)findViewById(R.id.id_signup);
         pwEdit = (EditText)findViewById(R.id.pw_signup);
         nameEdit = (EditText)findViewById(R.id.name_signup);
+        sameidBtn = (Button)findViewById(R.id.idsameBtn);
+        sameidBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hasSameId(idEdit.getText().toString());
+            }
+        });
         signupFinishBtn = (Button)findViewById(R.id.signupFinishBtn);
         signupFinishBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +78,24 @@ public class signupActivity extends AppCompatActivity {
                     Toast.makeText(signupActivity.this.getApplicationContext(),"회원가입 실패\n아이디,비밀번호,이름 중 기입하지 않은 것이 있습니다.",Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
+    private void hasSameId(String id){
+        ref = database.getReference("user/");
+        Query query = ref.orderByChild("id").equalTo(id);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                    Toast.makeText(signupActivity.this.getApplicationContext(),"아이디가 존재합니다.",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(signupActivity.this.getApplicationContext(),"입력하신 ID로 가능합니다.",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }

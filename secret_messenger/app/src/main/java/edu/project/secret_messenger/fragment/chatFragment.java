a139340b84aab7083f547673bd20a25f5a5a984a
kooym5607 <starkoo06@kookmin.ac.kr>
2020-07-23 10:22:25 +0900

    리스트 longClick 시 dialog 띄우기, 비밀메시지 부각

--- secret_messenger/app/src/main/java/edu/project/secret_messenger/fragment/chatFragment.java
+++ secret_messenger/app/src/main/java/edu/project/secret_messenger/fragment/chatFragment.java
@@ -1,6 +1,8 @@
 package edu.project.secret_messenger.fragment;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.util.Log;
@@ -45,19 +47,20 @@ public class chatFragment extends Fragment {
     private static final String TAG = "chatFragment";
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference ref;
-    private Query query;
+
+    private EditText chatEdit;
+    private EditText encKeyEdit;
+    private CheckBox enc_CheckBox;
+    private Button sendButton;
     private ListView listView;
+
     private ChatDTO chatDTO;
     private ArrayList<ChatDTO> chatDTOs = new ArrayList<ChatDTO>();
     private ChatListAdapter chatListAdapter;
     private String myID;
     private String mUserName;
-    private EditText chatEdit;
-    private EditText encKeyEdit;
     private String message;
-    private Button sendButton;
     private Date msg_Time;
-    private CheckBox enc_CheckBox;
     private String encKey;
     private boolean is_Enc;
 
@@ -74,6 +77,71 @@ public class chatFragment extends Fragment {
         sendButton = (Button)layout.findViewById(R.id.sendButton);
 
         myID = getArguments().getString("myID");
+        getUserName(myID);
+        Log.e(TAG,"내 이름은 "+mUserName);
+
+        ref = database.getReference("user").child(myID);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() { // 사용자 ID로부터 이름을 받아옴
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mUserName = snapshot.child("name").getValue(String.class);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+
+        ref = database.getReference("messages");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() { // 처음 채팅목록 받아오기
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(chatListAdapter!=null)
+                    chatListAdapter.clear();
+                for(DataSnapshot datas: snapshot.getChildren()){
+                    chatListAdapter.add(datas.getValue(ChatDTO.class));
+                }
+                chatListAdapter.registerDataSetObserver(new DataSetObserver() {
+                    @Override
+                    public void onChanged() {
+                        super.onChanged();
+                        listView.setSelection(chatListAdapter.getCount()-1);
+                    }
+                });
+                listView.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        ref.addChildEventListener(new ChildEventListener() { // 채팅이 추가/삭제 될 때 마다 리스트어댑터에 추가/삭제
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                chatListAdapter.add(snapshot.getValue(ChatDTO.class));
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                chatListAdapter.remove(snapshot.getValue(ChatDTO.class));
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() { // 채팅 길게 클릭했을 때 클릭 리스너
             @Override
@@ -82,33 +150,35 @@ public class chatFragment extends Fragment {
                 /** Todo 길게 클릭 시 삭제 / (암호문일 때 복호화하기) 메뉴 뜨게 하기 ;}
                  *
                  */
-                //메세지 삭제
-                if(chatDTOs.get(position).getUserID().equals(myID)){ // 선택한 채팅이 사용자가 작성한 것인지 확인 후 삭제.
-                    String msgUid =chatDTOs.get(position).getMsgUID();
-                    chatDTOs.remove(position);
-                    chatListAdapter.notifyDataSetChanged();
+                final int pos = position;
+                final CharSequence[] items = {"삭제","비밀메시지 보기"};
+                AlertDialog.Builder dialog = new AlertDialog.Builder(view.getContext(),android.R.style.Theme_DeviceDefault_Light_Dialog_Alert);
+                dialog.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        switch(i){
+                            case 0:
+                                if(chatDTOs.get(pos).getUserID().equals(myID)){ // 선택한 채팅이 사용자가 작성한 것인지 확인 후 삭제.
+                                    String msgUid =chatDTOs.get(pos).getMsgUID();
+                                    chatDTOs.remove(pos);
+                                    chatListAdapter.notifyDataSetChanged();
 
-                    ref = database.getReference("messages").child(msgUid);
-                    ref.removeValue();
+                                    ref = database.getReference("messages").child(msgUid);
+                                    ref.removeValue();
 
-                    listView.clearChoices();
-                }
-                else
-                    Toast.makeText(getContext(), "이 채팅의 사용자가 아니여서 삭제 불가", Toast.LENGTH_SHORT).show();
+                                    listView.clearChoices();
+                                }
+                                else
+                                    Toast.makeText(getContext(), "이 채팅의 사용자가 아니여서 삭제 불가", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 1:
+                                Toast.makeText(getContext(), "미구현", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                dialog.create().show();
 
                 return true;
-            }
-        });
-
-
-        ref = database.getReference("user").child(myID);
-        ref.addValueEventListener(new ValueEventListener() { // 사용자 ID로부터 이름을 받아옴
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                mUserName = snapshot.child("name").getValue(String.class);
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
             }
         });
 
@@ -144,55 +214,6 @@ public class chatFragment extends Fragment {
                 enc_CheckBox.setChecked(false);
             }
         });
-        ref = database.getReference("messages");
-        ref.addListenerForSingleValueEvent(new ValueEventListener() { // 처음 채팅목록 받아오기
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(chatListAdapter!=null)
-                    chatListAdapter.clear();
-                for(DataSnapshot datas: snapshot.getChildren()){
-                    chatListAdapter.add(datas.getValue(ChatDTO.class));
-                }
-                chatListAdapter.registerDataSetObserver(new DataSetObserver() {
-                    @Override
-                    public void onChanged() {
-                        super.onChanged();
-                        listView.setSelection(chatListAdapter.getCount()-1);
-                    }
-                });
-                listView.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        ref.addChildEventListener(new ChildEventListener() { // 채팅이 추가/삭제 될 때 마다 리스트어댑터에 추가/삭제
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                chatListAdapter.add(snapshot.getValue(ChatDTO.class));
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-                chatListAdapter.remove(snapshot.getValue(ChatDTO.class));
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
 
         return layout;
     }
@@ -222,9 +243,11 @@ public class chatFragment extends Fragment {
                 if(chatMsg != null){
                     if(chatDTO.getIs_Enc()==false){
                         chatMsg.setText(chatDTO.getMessage());
+                        view.setBackgroundResource(R.drawable.white_edittext);
                     }
                     else {
                         chatMsg.setText("비밀메시지입니다.");
+                        view.setBackgroundResource(R.drawable.red_edittext);
                     }
                 }
                 if(userName != null){
@@ -246,4 +269,18 @@ public class chatFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
     }
+
+    public void getUserName(String id){
+        ref = database.getReference("user").child(id);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() { // 사용자 ID로부터 이름을 받아옴
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mUserName = snapshot.child("name").getValue(String.class);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
 }
